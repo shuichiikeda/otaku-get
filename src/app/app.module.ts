397c7f7011a72244981525e31ed5db47f37aafc7
Masato Nagata <masatonagata@masatonagata.local>
2017-12-17 17:53:00 +0900

    selectページ追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { FukuiPage } from '../pages/fukui/fukui';
+import { SelectPage } from '../pages/select/select';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +20,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    FukuiPage
+    FukuiPage,
+    SelectPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    FukuiPage
+    FukuiPage,
+    SelectPage
   ],
   providers: [
     StatusBar,
